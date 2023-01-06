891fc165361f8c1e0a648f430fff8507e7b7a906
Isaac <fimbresisaac23@gmail.com>
2023-01-05 17:41:19 -0800

    deleting junk

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,7 +4,7 @@ import { SessionProvider } from "next-auth/react";
 
 import { api } from "../utils/api";
 
-import "../styles/globals.css";
+import "tailwindcss/tailwind.css";
 
 const MyApp: AppType<{ session: Session | null }> = ({
   Component,
